21da3ce64168f05c5f5087219b2262c429b55235
Miguel Ángel Sánchez Chordi <slayer.ma@gmail.com>
2017-04-18 17:04:05 +0200

    Initial commit

---
+++ src/FizzBuzz/FizzBuzz.php
@@ -0,0 +1,10 @@
+<?php
+namespace FizzBuzz;
+
+/**
+ * Class FizzBuzz
+ */
+class FizzBuzz
+{
+
+}
---
+++ src/FizzBuzz/Tests/FizzBuzzTest.php
@@ -0,0 +1,9 @@
+<?php
+namespace FizzBuzz\Tests;
+
+use FizzBuzz\FizzBuzz;
+
+class FizzBuzzTest extends \PHPUnit_Framework_TestCase
+{
+
+}
