a0d364fc3cbeccd3ddf041a34ef5e43ffad3e219
Miguel Ángel Sánchez Chordi <slayer.ma@gmail.com>
2017-04-19 17:30:02 +0200

    Clean Kata structure

---
+++ src/FizzBuzz/FizzBuzz.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace FizzBuzz;
+
+/**
+ * Class FizzBuzz
+ */
+class FizzBuzz
+{
+}
+
---
+++ src/FizzBuzz/Tests/FizzBuzzTest.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace FizzBuzz\Tests;
+
+use FizzBuzz\FizzBuzz;
+
+class FizzBuzzTest extends \PHPUnit_Framework_TestCase
+{
+}
+
